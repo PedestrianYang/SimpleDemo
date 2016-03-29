b38958c51dd1b1ca2b29e14379760769c0b43f3c
314510591 <ymq@ymqdeiMac.local>
2016-03-29 17:32:50 +0800

    [unowned self]防止闭包中循环引用

--- Swift_TableView/Swift_TableView/SecondViewController.swift
+++ Swift_TableView/Swift_TableView/SecondViewController.swift
@@ -12,8 +12,9 @@ import UIKit
 typealias callBack = (string:String)->Void
 
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     var funObj: callBack?
+    var _tableView :UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "2"
@@ -24,6 +25,12 @@ class SecondViewController: UIViewController {
         btn.addTarget(self, action:"btnClick", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(btn)
         // Do any additional setup after loading the view.
+        
+        _tableView = UITableView(frame: CGRectMake(0, 100, ScreenWith, ScreenHeight - 100), style: UITableViewStyle.Plain)
+        _tableView!.delegate = self
+        _tableView!.dataSource = self
+        _tableView!.registerClass(CustomCell.classForCoder(), forCellReuseIdentifier: "CELLID")
+        self.view.addSubview(_tableView!);
         
     }
 
@@ -37,6 +44,42 @@ class SecondViewController: UIViewController {
         funObj?(string: "111")
         print("click")
     }
+    
+    internal func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return 10;
+    }
+    
+    internal func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCellWithIdentifier("CELLID") as! CustomCell
+        cell.lab.text = "\(indexPath.row)"
+    
+        cell.click = {
+            //[unowned self] 标志，防止闭包循环引用，以致界面释放不掉
+             [unowned self] (titleStr: NSString) -> Void in
+            print("\(titleStr,self.title) \(indexPath.row)")
+        }
+        return cell;
+    }
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 300;
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        let secondVC = SecondViewController()
+        secondVC.funObj = {
+            (string:String)->Void in
+            print("\(string,self.title)")
+        };
+        self.navigationController?.pushViewController(secondVC, animated: true)
+    }
+    
+    deinit{
+        print("界面释放")
+    }
+    
     /*
     // MARK: - Navigation
 
